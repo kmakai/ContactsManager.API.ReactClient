6fc4231ac00e26830af8fab12684c1990c66500b
kmakai <makaikwag@gmail.com>
2023-09-27 20:13:32 -0400

    Add project files.

---
+++ ContactsManager.API.ReactClient/Repositories/IContactRespository.cs
@@ -0,0 +1,12 @@
+﻿using ContactsManager.API.ReactClient.Models;
+
+namespace ContactsManager.API.ReactClient.Repositories;
+
+public interface IContactRespository
+{
+    Task<IEnumerable<Contact>> GetContactsAsync();
+    Task<Contact> GetContactAsync(int id);
+    void AddContactAsync(Contact contact);
+    Task<Contact> UpdateContactAsync(Contact contact);
+    Task<Contact> DeleteContactAsync(int id);
+}
